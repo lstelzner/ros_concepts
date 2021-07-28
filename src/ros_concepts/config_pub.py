cd782c4099827488621a4824ccea6d22d1c1eac6
Laura Stelzner <lstelzner+github@gmail.com>
2021-07-27 18:02:31 -0700

    adding dynamic reconfig, and git ignore)

--- src/ros_concepts/config_pub.py
+++ src/ros_concepts/config_pub.py
@@ -2,6 +2,8 @@ import rospy
 import random
 from std_msgs.msg import Float64
 from std_srvs.srv import SetBool, SetBoolRequest, SetBoolResponse
+from dynamic_reconfigure.server import Server
+from ros_concepts.cfg import PublishSettingsConfig
 
 class ConfigurableFloatPub:
     """
@@ -9,10 +11,20 @@ class ConfigurableFloatPub:
     Has a service to enable/disable publishing
     """
     def __init__(self, topic_name="float"):
+        self.min_ = 0
+        self.max_ = 10000.
         self.rate_ = rospy.Rate(rospy.get_param("pub_frequency"))
         self.pub_ = rospy.Publisher(topic_name, Float64, queue_size=10)
         self.enabled_ = True
         self.srv_server_ = rospy.Service('enable_publish', SetBool, self.enable_cb)
+        self.dyn_server = Server(PublishSettingsConfig, self.dyn_reconfig_cb)
+
+    def dyn_reconfig_cb(self, config, level):
+        self.min_ = config.min
+        self.max_ = config.max
+        self.rate_ = rospy.Rate(config.frequency)
+        self.enabled_ = config.enable
+        return config
 
     def enable_cb(self, req):
         self.enabled_ = req.data
@@ -33,6 +45,6 @@ class ConfigurableFloatPub:
 
     def get_float(self):
         #Could be improved upon with precise range
-        return random.uniform(0, 100000.)
+        return random.uniform(self.min_, self.max_)
 
         
